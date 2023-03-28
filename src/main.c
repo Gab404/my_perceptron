c4fcac8b7829ffdffde4ab8c30e177a74d9eeb51
Gabriel Guiet-Dupré <gabriel.guiet-dupre@epitech.eu>
2023-03-28 17:28:54 +0200

    MODIF: display accuracy

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main(void)
     getData(network, "./data/iris-dataset.data");
 
     for (int i = 0; i < network->nbInput; i++)
-        network->neuron->w[i] = ((float)(rand() % (100 + 1 - 1) + 1)) / 100;
+        network->neuron->w[i] = ((float)(rand() % (50 + 1 - 1) + 1)) / 100;
 
     for (int i = 0; i < epochs; i++) { // train loop
         computeNetOut(network, network->trainData, network->lenDataTrain);
@@ -26,14 +26,11 @@ int main(void)
         printf("epoch = %d/%d\tloss = %f\n", i + 1, epochs, loss);
     }
 
-
     computeNetOut(network, network->testData, network->lenDataTest);
-    for (int i = 0; i < network->lenDataTest; i++) {
-        // printf("%i | pred = %f | result = %f\n", i, network->networkOut[i], network->labelTest[i]);
+    for (int i = 0; i < network->lenDataTest; i++)
         if (roundf(network->networkOut[i]) == network->labelTest[i])
             goodPred++;
-    }
-    printf("Accuracy = %d%%\n", (goodPred * 100) / network->lenDataTest);
+    printf("\n----Accuracy = %d%%----\n\n", (goodPred * 100) / network->lenDataTest);
 
     freeNetwork(network);
     return 0;
